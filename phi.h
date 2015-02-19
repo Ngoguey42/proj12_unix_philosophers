7aacd9837750d95d66249cfc6a567fd3e24c28ee
wide-aze <wide-aze@student.42.fr>
2015-02-19 11:00:03 +0100

    test

--- phi.h
+++ phi.h
@@ -6,7 +6,7 @@
 /*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/19 10:12:28 by ngoguey           #+#    #+#             */
-/*   Updated: 2015/02/19 10:44:10 by ngoguey          ###   ########.fr       */
+/*   Updated: 2015/02/19 10:58:57 by wide-aze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@
 
 typedef struct	s_env
 {
-	int lol2
-	int lol;
-	int lol2;
-
 }				t_env;
 
 
