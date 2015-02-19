0c0217298f99a19510707733438bb851fb064d3c
Ngoguey42 <ngoguey@student.42.fr>
2015-02-19 12:48:44 +0100

    work in progress

--- phi.h
+++ phi.h
@@ -6,7 +6,7 @@
 /*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/19 10:12:28 by ngoguey           #+#    #+#             */
-/*   Updated: 2015/02/19 12:29:11 by wide-aze         ###   ########.fr       */
+/*   Updated: 2015/02/19 12:48:00 by ngoguey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,27 @@
 # include <ft_math.h>
 #include <ft_debug.h> //debug
 
+# define MAX_LIFE 100
+# define EAT_T 5
+# define REST_T 3
+# define THINK_T 2
+# define TIMEOUT 30
 
-typedef enum	s_state
+typedef enum	s_pstat
 {
 	rest = 0,
 	think = 1,
 	eat = 2,
-}				t_state;
+	waiteat = 3,
+	waitthink = 4,
+}				t_pstat;
 
+typedef enum	s_sstat
+{
+	free = 0,
+	left = 2,
+	right = 3,
+}				t_sstat;
 
 /*
 ** *****************************************************************************
@@ -62,6 +75,11 @@ typedef struct	s_graph
 typedef struct	s_env
 {
 	t_graph		g;
+	t_sstat		stick_s[7];
+	int			mutex[7];
+	t_pstat		phi_s[7];
+	int			phi_hp[7];	
+
 }				t_env;
 typedef CS_ENV	t_cenv;
 
