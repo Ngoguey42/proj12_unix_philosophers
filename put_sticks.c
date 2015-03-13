85e7e4f5f3e939f4826d887645381f365f1b14e1
Ngoguey42 <ngoguey@student.42.fr>
2015-03-13 10:35:27 +0100

    norm on put_stick

--- put_sticks.c
+++ put_sticks.c
@@ -6,7 +6,7 @@
 /*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/20 12:37:38 by ngoguey           #+#    #+#             */
-/*   Updated: 2015/02/27 17:24:32 by wide-aze         ###   ########.fr       */
+/*   Updated: 2015/03/13 10:35:05 by ngoguey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,30 +42,34 @@
 #define T_S t_stick
 #define P_VCOTOI(R, G, B, A) (t_co){{B, G, R, A}}
 
-static t_stick	tstick(int i) //ACO fait fail la nm
+/*
+** had to remove (t_cooi) cast inside table below because of norminette.
+*/
+
+static t_stick	tstick(int i)
 {
 	const t_stick		s[21] = {
 
 	(const T_S){P_ACOOADD(P0_POS, PR + HSS, PR2 + PTP + SPU), 0, 0, v},
-	(const T_S){(t_cooi){LP + TR + HTR, TP + SPA, 0}, 0, -1, v},
+	(const T_S){{LP + TR + HTR, TP + SPA, 0}, 0, -1, v},
 	(const T_S){P_ACOOADD(P1_POS, -PTP - SPU - SL, PR - HSS), 0, 1, h},
 	(const T_S){P_ACOOADD(P1_POS, -PTP - SPU - SL, PR + HSS), 1, 1, h},
-	(const T_S){(t_cooi){LP + TR2 - SPA - SL, TP + TR - 69, 0}, 1, -1, h},
+	(const T_S){{LP + TR2 - SPA - SL, TP + TR - 69, 0}, 1, -1, h},
 	(const T_S){P_ACOOADD(P2_POS, -PTP - SPU - SL, PR - HSS), 1, 2, h},
 	(const T_S){P_ACOOADD(P2_POS, -PTP - SPU - SL, PR + HSS), 2, 2, h},
-	(const T_S){(t_cooi){LP + TR2 - SPA - SL, TP + TR + HTR + 20, 0}, 2, -1, h},
+	(const T_S){{LP + TR2 - SPA - SL, TP + TR + HTR + 20, 0}, 2, -1, h},
 	(const T_S){P_ACOOADD(P3_POS, PR + HSS, -PTP - SPU - SL), 2, 3, v},
 	(const T_S){P_ACOOADD(P3_POS, PR - HSS, -PTP - SPU - SL), 3, 3, v},
-	(const T_S){(t_cooi){LP + TR, TP + TR2 - SPA - SL, 0}, 3, -1, v},
+	(const T_S){{LP + TR, TP + TR2 - SPA - SL, 0}, 3, -1, v},
 	(const T_S){P_ACOOADD(P4_POS, PR + HSS, -PTP - SPU - SL), 3, 4, v},
 	(const T_S){P_ACOOADD(P4_POS, PR - HSS, -PTP - SPU - SL), 4, 4, v},
-	(const T_S){(t_cooi){LP + SPA, TP + TR + HTR + 20, 0}, 4, -1, h},
+	(const T_S){{LP + SPA, TP + TR + HTR + 20, 0}, 4, -1, h},
 	(const T_S){P_ACOOADD(P5_POS, PR2 + PTP + SPU, PR + HSS), 4, 5, h},
 	(const T_S){P_ACOOADD(P5_POS, PR2 + PTP + SPU, PR - HSS), 5, 5, h},
-	(const T_S){(t_cooi){LP + SPA, TP + TR - 69, 0}, 5, -1, h},
+	(const T_S){{LP + SPA, TP + TR - 69, 0}, 5, -1, h},
 	(const T_S){P_ACOOADD(P6_POS, PR2 + PTP + SPU, PR + HSS), 5, 6, h},
 	(const T_S){P_ACOOADD(P6_POS, PR2 + PTP + SPU, PR - HSS), 6, 6, h},
-	(const T_S){(t_cooi){LP + HTR, TP + SPA, 0}, 6, -1, v},
+	(const T_S){{LP + HTR, TP + SPA, 0}, 6, -1, v},
 	(const T_S){P_ACOOADD(P0_POS, PR - HSS, PR2 + PTP + SPU), 6, 0, v}
 	};
 	return ((t_stick)s[i]);
@@ -88,6 +92,28 @@ static void		put_it(const t_graph *g, t_cooi coo, t_co c, int index)
 	return ;
 }
 
+static void		put_left(t_cenv *e, int i, t_stick s)
+{
+	if (e->llock[s.owner] == think_with || e->llock[s.owner] == eat_with)
+		put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 210, 30, 0), i);
+	else if (e->llock[s.owner] == stolen)
+		put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 50, 30, 0), i);
+	else if (e->llock[s.owner] == waited)
+		put_it(&e->g, tstick(i).coo, P_VCOTOI(0x66, 0xCC, 0xCC, 0), i);
+	return ;
+}
+
+static void		put_right(t_cenv *e, int i, t_stick s)
+{
+	if (e->rlock[s.owner] == think_with || e->rlock[s.owner] == eat_with)
+		put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 210, 30, 0), i);
+	else if (e->rlock[s.owner] == stolen)
+		put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 50, 30, 0), i);
+	else if (e->rlock[s.owner] == waited)
+		put_it(&e->g, tstick(i).coo, P_VCOTOI(0x66, 0xCC, 0xCC, 0), i);
+	return ;
+}
+
 void			phi_put_sticks(t_cenv *e)
 {
 	int			i;
@@ -98,30 +124,12 @@ void			phi_put_sticks(t_cenv *e)
 	{
 		s = tstick(i);
 		if (i % 3 == 0)
-		{
-			if (e->llock[s.owner] == think_with || e->llock[s.owner] == eat_with)
-				put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 210, 30, 0), i);
-			else if (e->llock[s.owner] == stolen)
-				put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 50, 30, 0), i);
-			else if (e->llock[s.owner] == waited)
-				put_it(&e->g, tstick(i).coo, P_VCOTOI(0x66, 0xCC, 0xCC, 0), i);
-		}
+			put_left(e, i, s);
 		else if (i % 3 == 2)
-		{
-			if (e->rlock[s.owner] == think_with || e->rlock[s.owner] == eat_with)
-				put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 210, 30, 0), i);
-			else if (e->rlock[s.owner] == stolen)
-				put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 50, 30, 0), i);
-			else if (e->rlock[s.owner] == waited)
-				put_it(&e->g, tstick(i).coo, P_VCOTOI(0x66, 0xCC, 0xCC, 0), i);
-		}
-		else
-		{
-			if (e->owner[s.stick_id] == s.owner)
-				put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 210, 30, 0), i);
-		}
+			put_right(e, i, s);
+		else if (e->owner[s.stick_id] == s.owner)
+			put_it(&e->g, tstick(i).coo, P_VCOTOI(210, 210, 30, 0), i);
 		i++;
 	}
 	return ;
 }
-
