608f57e91fdeffe819c49d1cf16335837141d78e
Ngoguey42 <ngoguey@student.42.fr>
2015-03-27 05:36:20 +0100

    truc

--- philo_pick_event.c
+++ philo_pick_event.c
@@ -24,6 +24,30 @@ void						phi_can_wait_to_eat_pick(t_env *e, int id)
 
 void						phi_cant_wait_to_eat_pick(t_env *e, int id)
 {
-	phi_start_end_event(e, id);
+	// rest
+	// wait think
+	// think
+	// eat
+	if (!pthread_mutex_trylock(&e->mutex[P_LSID(id)]))
+	{
+		e->llock[id] = eat_with;
+		if (!pthread_mutex_trylock(&e->mutex[P_RSID(id)]))
+		{
+			e->rlock[id] = eat_with;
+			phi_waiteat_start_event(e, id);
+			return ;
+		}
+		if (e->official_s[P_RPID(id)] == think ||
+				e->official_s[P_RPID(id)] == wthink)
+		{
+			phi_waiteat_start_event(e, id);
+			return ;
+		}
+		e->llock[id] = think_with;
+		e->think_stick[id] = P_LSID(id);
+		phi_waitthink_start_event(e, id);
+		return ;
+	}
+	phi_rest_start_event(e, id);
 	return ;
 }
